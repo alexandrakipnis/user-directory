c28845afb926ec2ab43d198111c1d992de3cf5eb
Sasha Kipnis <akipnis@purdue.edu>
2018-05-14 14:29:56 -0400

    edited headers

--- index.js
+++ index.js
@@ -3,18 +3,18 @@ const button = document.querySelector('button')
 const header = document.querySelector('h1.header')
 const header2 = document.querySelector('h1.second')
 
-button.addEventListener('click', function(ev){
-    header.textContent = "You clicked the button!"
-    header2.textContent = "My last name is Kipnis"
-    ev.target.textContent = "AHHHH CLICKED"
+const form = document.getElementById('form')
+const submitted = document.form.submitbutton
+
+function getMessage(){
+    const name = document.getElementsById("message")[0].value;
+}
+
+submitted.addEventListener('click', function(ev){
+    header2.textContent = name
 })
 
-
-
-
-// homework: make the button change the text of the heading
-
-// bonus credit: add multiple headings to the page and make the button change the second one 
-
-// super mega bonus credit: add a form to the page, add a text input to the form, update the 
-//             heading with the text that you type in the text input 
+button.addEventListener('click', function(ev){
+    header2.textContent = "You clicked the button!"
+    ev.target.textContent = "AHHHH CLICKED"
+})
