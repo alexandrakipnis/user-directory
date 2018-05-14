7572f372f173263a1d899b9bb4f65bb7fa26b466
Sasha Kipnis <akipnis@purdue.edu>
2018-05-14 13:28:21 -0400

    Button click changes headers

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("it works!")
+
+const button = document.querySelector('button')
+const header = document.querySelector('h1.header')
+const header2 = document.querySelector('h1.second')
+
+button.addEventListener('click', function(ev){
+    header.textContent = "You clicked the button!"
+    header2.textContent = "My last name is Kipnis"
+    ev.target.textContent = "AHHHH CLICKED"
+})
+
+
+
+
+// homework: make the button change the text of the heading
+
+// bonus credit: add multiple headings to the page and make the button change the second one 
+
+// super mega bonus credit: add a form to the page, add a text input to the form, update the 
+//             heading with the text that you type in the text input 
