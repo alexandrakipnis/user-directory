196d3808f7072f3aea00812d684a13b42668b8a6
Sasha Kipnis <akipnis@purdue.edu>
2018-05-14 14:51:44 -0400

    Fixed form function and edited appearance

--- index.js
+++ index.js
@@ -1,20 +1,14 @@
 
 const button = document.querySelector('button')
-const header = document.querySelector('h1.header')
+//const header = document.querySelector('h1.header')
 const header2 = document.querySelector('h1.second')
 
-const form = document.getElementById('form')
-const submitted = document.form.submitbutton
 
-function getMessage(){
-    const name = document.getElementsById("message")[0].value;
-}
-
-submitted.addEventListener('click', function(ev){
-    header2.textContent = name
-})
 
 button.addEventListener('click', function(ev){
+    let value = document.querySelector('input').value
+    let header = document.querySelector('#header')
     header2.textContent = "You clicked the button!"
     ev.target.textContent = "AHHHH CLICKED"
+    header.textContent = value;
 })
