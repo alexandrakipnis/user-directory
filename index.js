ee8a5073c0b7f213cffcda7e1e15324a0300afde
Sasha Kipnis <akipnis@purdue.edu>
2018-05-15 10:57:01 -0400

    Add age and form reset

--- index.js
+++ index.js
@@ -21,12 +21,15 @@ const handleSubmit = function(ev){
     ev.preventDefault()
     const form = ev.target
     const userName = form.userName.value
+    const age = form.age.value
     const users = document.querySelector('#users')
-    users.innerHTML += '<p>' + userName + '</p>'
+    users.innerHTML += '<p>' + userName + ', ' + age + '</p>'
     form.userName.value = ' '
 
     const header2 = document.querySelector('h1.second')
     header2.textContent = userName
+
+    form.reset()
 }
 
 form.addEventListener('submit', handleSubmit)
