90a96da9fb69a11476367f778ca72ce601cbeb02
Sasha Kipnis <akipnis@purdue.edu>
2018-05-15 11:07:32 -0400

    Use a template literal instead of old school string concatenation

--- index.js
+++ index.js
@@ -1,29 +1,13 @@
-
-/*const button = document.querySelector('button')
-//const header = document.querySelector('h1.header')
-const header2 = document.querySelector('h1.second')
-
-
-
-button.addEventListener('click', function(ev){
-    let value = document.querySelector('input').value
-    let header = document.querySelector('#users')
-    header.textContent = "You clicked the button!"
-    ev.target.textContent = "AHHHH CLICKED!!"
-    header2.textContent = value;
-    
-    const users = document.querySelector('#users')
-    users.textContent += '' + value
-})*/
-
 const form = document.querySelector('form#userForm')
+
 const handleSubmit = function(ev){
     ev.preventDefault()
     const form = ev.target
     const userName = form.userName.value
     const age = form.age.value
     const users = document.querySelector('#users')
-    users.innerHTML += '<p>' + userName + ', ' + age + '</p>'
+    //users.innerHTML += '<p>' + userName + ', ' + age + '</p>'
+    users.innerHTML += `<p>${userName}, ${age}</p>`
     form.userName.value = ' '
 
     const header2 = document.querySelector('h1.second')
