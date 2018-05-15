a1971f3abf3b288f304d5b703b8dcb4baa8b9649
Sasha Kipnis <akipnis@purdue.edu>
2018-05-15 09:53:11 -0400

    Update the button appearance

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const header2 = document.querySelector('h1.second')
 button.addEventListener('click', function(ev){
     let value = document.querySelector('input').value
     let header = document.querySelector('#header')
-    header2.textContent = "You clicked the button!"
+    header.textContent = "You clicked the button!"
     ev.target.textContent = "AHHHH CLICKED"
-    header.textContent = value;
+    header2.textContent = value;
 })
