d9df1d40e4d479da8a8599062a251e27a7c7c112
Sasha Kipnis <akipnis@purdue.edu>
2018-05-16 11:07:09 -0400

    Make user object in renderListItem

--- index.js
+++ index.js
@@ -20,32 +20,55 @@ const renderListItem = function(label, item){
     } catch(e){
         content.textContent += item
     }
-    
+
     return content
 }
 
 //create list item with user information
 const renderList = function(userName, age, favoriteColor){
     const list = document.createElement('ul')
+
+    const user = {
+        'Name: ': userName,
+        'Age: ': age,
+        'Favorite Color: ': renderColor(favoriteColor)
+    }
+
+    Object.keys(user).map(function(label) {
+        list.appendChild(renderListItem(label, user[label]))
+    })
+
+    /*
     list.appendChild(renderListItem("Name: ", userName))
     list.appendChild(renderListItem("Age: ", age))
     const colorItem = renderListItem("Favorite Color", '')
     colorItem.appendChild(renderColor(favoriteColor))
-    list.appendChild(colorItem)
+    list.appendChild(colorItem)*/
+
     return list
 }
 
 const handleSubmit = function(ev){
     ev.preventDefault()
 
-    //declaring users variables
     const form = ev.target
+
+    /*const user = {
+        name: form.userName.value,
+        age: form.age.value,
+        favoriteColor = form.favoriteColor.value
+    }*/
+
+    const users = document.querySelector('#users')
+
+    /*Object.keys(user).map(function(label) {
+        users.appendChild(renderListItem(label, user[label]))
+    })*/
+
+    //declaring users variables
     const userName = form.userName.value
     const age = form.age.value
     const favoriteColor = form.favoriteColor.value
-
-    //list of users
-    const users = document.querySelector('#users')
 
     //append new user to list of users
     users.appendChild(renderList(userName, age, favoriteColor))
